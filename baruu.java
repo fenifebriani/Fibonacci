fd604c04a82cc4f55a26b1a923b8ab549b4200fa
feni febriani <49176326+fenifebriani@users.noreply.github.com>
2019-04-02 09:06:55 +0700

    Add files via upload

---
+++ baruu.java
@@ -0,0 +1,21 @@
+public class baruu {
+    
+        public static void main(String[] args)
+        {
+
+            int maxNumber = 10, previousNumber = 0, nextNumber = 1;
+            System.out.print("Fibonacci Series of "+maxNumber+" numbers:");
+
+            int i=1;
+            while(i <= maxNumber)
+            {
+                System.out.print(previousNumber+" ");
+                int sum = previousNumber + nextNumber;
+                previousNumber = nextNumber;
+                nextNumber = sum;
+                i++;
+            }
+
+        }
+
+    }
